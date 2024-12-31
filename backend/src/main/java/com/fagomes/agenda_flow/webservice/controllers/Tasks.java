b492c8e314d2056bcda11058f988d6bd502b9918
fagomes <fagomes13@gmail.com>
2024-12-31 18:41:54 -0300

    configurated the dockerfile

--- backend/src/main/java/com/fagomes/agenda_flow/webservice/controllers/Tasks.java
+++ backend/src/main/java/com/fagomes/agenda_flow/webservice/controllers/Tasks.java
@@ -2,7 +2,6 @@ package com.fagomes.agenda_flow.webservice.controllers;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping(path = "/api")
