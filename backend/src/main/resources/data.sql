b567fb2ae9de8ab5a51cc575a6040161ab07b5c7
fagomes <fagomes13@gmail.com>
2025-01-03 21:10:51 -0300

    configuration

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,11 +1,31 @@
-insert into users (
-    id,
-    name,
-    password,
-    email
-) values (
-    1,
-    'teste',
-    'teste',
-    'teste'
-);
+-- INSERT INTO
+--     users (id, name, password, email)
+-- VALUES
+--     (
+--         9361,
+--         'Ketlin P. Nunes',
+--         'ket123',
+--         'kpecharka@gmail.com'
+--     );
+
+-- INSERT INTO tasks (
+--     id,
+--     title,
+--     description,
+--     due_at,
+--     created_at,
+--     completed_at,
+--     deleted_at,
+--     user_id
+-- ) VALUES (
+--     1,
+--     'Primeira task',
+--     'Essa e a primeira task do meu site, e eu estou muuuito feliz :) <3',
+--     '2025-01-20T10:10',
+--     '2025-01-01T10:10',
+--     NULL,
+--     NULL,
+--     9361
+-- );
+
+SELECT * FROM tasks;
