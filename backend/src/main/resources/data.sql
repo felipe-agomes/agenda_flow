dee61e9ac4fc2e34d16dd68d34517a7db0d016fb
fagomes <fagomes13@gmail.com>
2025-01-08 22:53:39 -0300

    No code change

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,31 +1,31 @@
-INSERT INTO
-    users (id, name, password, email)
-VALUES
-    (
-        9361,
-        'Ketlin P. Nunes',
-        'ket123',
-        'kpecharka@gmail.com'
-    );
+-- INSERT INTO
+--     users (id, name, password, email)
+-- VALUES
+--     (
+--         9361,
+--         'Ketlin P. Nunes',
+--         'ket123',
+--         'kpecharka@gmail.com'
+--     );
 
-INSERT INTO tasks (
-    id,
-    title,
-    description,
-    due_at,
-    created_at,
-    completed_at,
-    deleted_at,
-    user_id
-) VALUES (
-    1,
-    'Primeira task',
-    'Essa e a primeira task do meu site, e eu estou muuuito feliz :) <3',
-    '2025-01-20T10:10',
-    '2025-01-01T10:10',
-    NULL,
-    NULL,
-    9361
-);
+-- INSERT INTO tasks (
+--     id,
+--     title,
+--     description,
+--     due_at,
+--     created_at,
+--     completed_at,
+--     deleted_at,
+--     user_id
+-- ) VALUES (
+--     1,
+--     'Primeira task',
+--     'Essa e a primeira task do meu site, e eu estou muuuito feliz :) <3',
+--     '2025-01-20T10:10',
+--     '2025-01-01T10:10',
+--     NULL,
+--     NULL,
+--     9361
+-- );
 
--- SELECT * FROM tasks;
+SELECT * FROM tasks;
